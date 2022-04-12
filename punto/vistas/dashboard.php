5c32a6afd3a7bb893f8b8c75416248f989ccc463
ferdvl98 <ferdvl98@gmail.com>
2022-04-11 19:36:27 -0500

    h

--- punto/vistas/dashboard.php
+++ punto/vistas/dashboard.php
@@ -19,6 +19,9 @@
 <!-- Main content -->
 <div class="content">
     <div class="container-fluid">
+        <div class="row">
+            
+        </div>
 
     </div><!-- /.container-fluid -->
 </div>
